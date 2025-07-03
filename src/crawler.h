cb26b7f3d8af887fe7828a12df78b4fa120c29b3
Chibioi <ezehstanley2002@gmail.com>
2025-07-03 22:23:10 +0100

    Update

--- src/crawler.h
+++ src/crawler.h
@@ -2,7 +2,57 @@
 #define CRAWLER_H
 
 #include <stdio.h>
+#include <time.h>
 
 #define DEFAULT_FETCH_TIMEOUT_MS 10000 // Default fetcher timeout before givingup an URL
+                                       //
 #define DEFAULT_CRAWL_TIMEOUT_MS 30000 // default crawling timeout, time to wait and stop the crawl if no links are found
-#define DEFAULT_POLITENESS_DELAY_MS 500
+                                       //
+#define DEFAULT_POLITENESS_DELAY_MS 500 
+// Default politeness delay, fixed delay to calculate a randomized wait time
+// for subsequent HTTP calls to a domain (in milliseconds)
+//
+#define DEFAULT_DEPTH 16
+// default depth to crawl for each domain
+
+#define DEFAULT_CONCURRENCY 8
+// default number of concurrent threads to crawl 
+
+#define DEFAULT_USER_AGENT "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+// Default user agent to use
+
+typedef struct {
+    int status_code;
+} http_response_t;
+
+typedef struct {
+    char* full_url;
+} url_t;
+
+typedef struct {
+    void* data;
+} fetcher_parser_t;
+
+
+typedef int (*fetch_func)(const char* url_str, time_t* duration_ms, http_response_t** response);
+
+typedef struct {
+    fetch_func fetch;
+} fetcher;
+
+typedef int (*fetch_links_func)(const char* url_str, time_t* duration_ms, url_t*** links_out, int* num_links_out);
+
+typedef struct {
+    fetcher base_fetcher; // Embedding the base Fetcher
+    fetch_links_func fetch_links;
+    // Internal data for the LinkFetcher, e.g., user agent, parser.
+    char* user_agent;
+    fetcher_parser_t* parser;
+    time_t fetch_timeout_ms;
+} LinkFetcher;
+
+
+
+
+
+
--- src/jotter/jotter.c
+++ src/jotter/jotter.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
-#define min(a, b) (((a) < (b)) ? a : b)
+struct A {
+    int x;
+};
 
-int main() {
-    int a = 27;
-    int b = 30;
-
-    printf("Minimum: %d\n", min(a, b));
-    return 0;
-        
+void increment(struct A a, struct A *b) {
+    a.x++;
+    b->x++;
 }
 
+int main() {
+
+    struct A a = {10};
+    struct A b = {10};
+
+    increment(a, &b);
+
+    printf("a.x: %d \tb.x: %d\n", a.x, b.x);
+
+    return 0;
+}
